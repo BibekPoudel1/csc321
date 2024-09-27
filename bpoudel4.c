bff45aab14432a618dba6f7eb1509d8e75be0a12
BibekPoudel1 <bpoudel1@toromail.csudh.edu>
2024-09-27 16:11:31 +0000

    Lab #4 First Version

---
+++ bpoudel4.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(){
+
+	int x;
+	printf("Enter an Integer: ");
+	scanf("%d", &x);
+
+	if (x>1){
+		printf("Integer variable is greater than 1\n");
+	}
+	else{
+		printf("Integer variable is not greater than 1\n");
+	}
+
+	// Float
+	float y;
+	printf("Enter a float : ");
+	scanf("%f", &y);
+	
+	if (y>2.5){
+		printf("The float variable is greater than 2.5\n");
+	}else{
+		printf("The float variable is less than or equal to 2.5\n");
+	}
+
+	// String
+	char s[100]; // the variable holds string of length 100
+	printf("Enter a string:");
+	scanf("%s", s);
+	
+	if (strcmp(s,"Hello World") == 0){
+		printf("String entered is equal to 'Hello World'\n");
+	}else{
+		printf("The string is not equal to Hello World\n");
+	}
+
+	//Boolean
+	bool b;
+	int i;
+	printf("Enter 1 for True and 0 for False: \n");
+	scanf("%d", &i);
+	if (i == 0){
+		b = false;
+	}else if (i == 1){
+		b = true;
+	}else{
+		printf("Invalid input. Please enter 0 or 1\n");
+	}
+
+	if (b){
+		printf("You entered true\n");
+	}else{
+		printf("You entered false\n");
+	}
+
+
+		
+	
+}
